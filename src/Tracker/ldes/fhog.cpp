1d24765005ab591eb0d8f03de92af4a5bc908ec6
Nuzhny007 <nuzhny@mail.ru>
2019-12-14 23:32:00 +0300

    Remove dependencies from deprecated OpenCV C API

--- src/Tracker/ldes/fhog.cpp
+++ src/Tracker/ldes/fhog.cpp
@@ -68,7 +68,7 @@
 // Getting feature map for the selected subimage
 //
 // API
-// int getFeatureMaps(const IplImage * image, const int k, featureMap **map);
+// int getFeatureMaps(const cv::Mat& image, const int k, featureMap **map);
 // INPUT
 // image             - selected subimage
 // k                 - size of cells
@@ -77,80 +77,58 @@
 // RESULT
 // Error status
 */
-int getFeatureMaps(const IplImage* image, const int k, CvLSVMFeatureMapCaskade **map)
+int getFeatureMaps(const cv::Mat& image, const int k, CvLSVMFeatureMapCaskade **map)
 {
-    int sizeX, sizeY;
-    int p, px, stringSize;
-    int height, width, numChannels;
-    int i, j, kk, c, ii, jj, d;
-    float  * datadx, * datady;
-    
-    int   ch; 
-    float magnitude, x, y, tx, ty;
-    
-    IplImage * dx, * dy;
-    int *nearest;
-    float *w, a_x, b_x;
-
     float kernel[3] = {-1.f, 0.f, 1.f};
-    CvMat kernel_dx = cvMat(1, 3, CV_32F, kernel);
-    CvMat kernel_dy = cvMat(3, 1, CV_32F, kernel);
+    cv::Mat kernel_dx(1, 3, CV_32F, kernel);
+    cv::Mat kernel_dy(3, 1, CV_32F, kernel);
 
-    float * r;
-    int   * alfa;
-    
     float boundary_x[NUM_SECTOR + 1];
     float boundary_y[NUM_SECTOR + 1];
-    float max, dotProd;
-    int   maxi;
 
-    height = image->height;
-    width  = image->width ;
+    int height = image.rows;
+    int width  = image.cols;
 
-    numChannels = image->nChannels;
+    int numChannels = image.channels();
 
-    dx    = cvCreateImage(cvSize(image->width, image->height), 
-                          IPL_DEPTH_32F, 3);
-    dy    = cvCreateImage(cvSize(image->width, image->height), 
-                          IPL_DEPTH_32F, 3);
-
-    sizeX = width  / k;
-    sizeY = height / k;
-    px    = 3 * NUM_SECTOR; 
-    p     = px;
-    stringSize = sizeX * p;
+    int sizeX = width  / k;
+    int sizeY = height / k;
+    int px    = 3 * NUM_SECTOR; 
+    int p     = px;
+    int stringSize = sizeX * p;
     allocFeatureMapObject(map, sizeX, sizeY, p);
 
-    cvFilter2D(image, dx, &kernel_dx, cvPoint(-1, 0));
-    cvFilter2D(image, dy, &kernel_dy, cvPoint(0, -1));
+	cv::Mat dx;
+	cv::Mat dy;
+	cv::filter2D(image, dx, CV_32FC3, kernel_dx, cv::Point(-1, 0));
+	cv::filter2D(image, dy, CV_32FC3, kernel_dy, cv::Point(0, -1));
     
-    float arg_vector;
-    for(i = 0; i <= NUM_SECTOR; i++)
+    for (int i = 0; i <= NUM_SECTOR; i++)
     {
-        arg_vector    = ( (float) i ) * ( (float)(PI) / (float)(NUM_SECTOR) );
+        float arg_vector    = ( (float) i ) * ( (float)(PI) / (float)(NUM_SECTOR) );
         boundary_x[i] = cosf(arg_vector);
         boundary_y[i] = sinf(arg_vector);
     }/*for(i = 0; i <= NUM_SECTOR; i++) */
 
-    r    = (float *)malloc( sizeof(float) * (width * height));
-    alfa = (int   *)malloc( sizeof(int  ) * (width * height * 2));
+    float* r  = (float *)malloc( sizeof(float) * (width * height));
+    int* alfa = (int   *)malloc( sizeof(int  ) * (width * height * 2));
 
-    for(j = 1; j < height - 1; j++)
+    for (int j = 1; j < height - 1; j++)
     {
-        datadx = (float*)(dx->imageData + dx->widthStep * j);
-        datady = (float*)(dy->imageData + dy->widthStep * j);
-        for(i = 1; i < width - 1; i++)
+        const float* datadx = dx.ptr<float>(j);
+		const float* datady = dy.ptr<float>(j);
+        for (int i = 1; i < width - 1; i++)
         {
-            c = 0;
-            x = (datadx[i * numChannels + c]);
-            y = (datady[i * numChannels + c]);
+            int c = 0;
+            float x = (datadx[i * numChannels + c]);
+            float y = (datady[i * numChannels + c]);
 
             r[j * width + i] =sqrtf(x * x + y * y);
-            for(ch = 1; ch < numChannels; ch++)
+            for(int ch = 1; ch < numChannels; ch++)
             {
-                tx = (datadx[i * numChannels + ch]);
-                ty = (datady[i * numChannels + ch]);
-                magnitude = sqrtf(tx * tx + ty * ty);
+                float tx = (datadx[i * numChannels + ch]);
+                float ty = (datady[i * numChannels + ch]);
+                float magnitude = sqrtf(tx * tx + ty * ty);
                 if(magnitude > r[j * width + i])
                 {
                     r[j * width + i] = magnitude;
@@ -160,11 +138,11 @@ int getFeatureMaps(const IplImage* image, const int k, CvLSVMFeatureMapCaskade *
                 }
             }/*for(ch = 1; ch < numChannels; ch++)*/
             
-            max  = boundary_x[0] * x + boundary_y[0] * y;
-            maxi = 0;
-            for (kk = 0; kk < NUM_SECTOR; kk++) 
+            float max = boundary_x[0] * x + boundary_y[0] * y;
+            int maxi = 0;
+            for (int kk = 0; kk < NUM_SECTOR; kk++) 
             {
-                dotProd = boundary_x[kk] * x + boundary_y[kk] * y;
+                float dotProd = boundary_x[kk] * x + boundary_y[kk] * y;
                 if (dotProd > max) 
                 {
                     max  = dotProd;
@@ -184,47 +162,47 @@ int getFeatureMaps(const IplImage* image, const int k, CvLSVMFeatureMapCaskade *
         }/*for(i = 0; i < width; i++)*/
     }/*for(j = 0; j < height; j++)*/
 
-    nearest = (int  *)malloc(sizeof(int  ) *  k);
-    w       = (float*)malloc(sizeof(float) * (k * 2));
+    int* nearest = (int  *)malloc(sizeof(int  ) *  k);
+    float* w     = (float*)malloc(sizeof(float) * (k * 2));
     
-    for(i = 0; i < k / 2; i++)
+    for (int i = 0; i < k / 2; i++)
     {
         nearest[i] = -1;
     }/*for(i = 0; i < k / 2; i++)*/
-    for(i = k / 2; i < k; i++)
+    for (int i = k / 2; i < k; i++)
     {
         nearest[i] = 1;
     }/*for(i = k / 2; i < k; i++)*/
 
-    for(j = 0; j < k / 2; j++)
+    for (int j = 0; j < k / 2; j++)
     {
-        b_x = k / 2 + j + 0.5f;
-        a_x = k / 2 - j - 0.5f;
+        float b_x = k / 2 + j + 0.5f;
+        float a_x = k / 2 - j - 0.5f;
         w[j * 2    ] = 1.0f/a_x * ((a_x * b_x) / ( a_x + b_x)); 
         w[j * 2 + 1] = 1.0f/b_x * ((a_x * b_x) / ( a_x + b_x));  
     }/*for(j = 0; j < k / 2; j++)*/
-    for(j = k / 2; j < k; j++)
+    for (int j = k / 2; j < k; j++)
     {
-        a_x = j - k / 2 + 0.5f;
-        b_x =-j + k / 2 - 0.5f + k;
+        float a_x = j - k / 2 + 0.5f;
+        float b_x =-j + k / 2 - 0.5f + k;
         w[j * 2    ] = 1.0f/a_x * ((a_x * b_x) / ( a_x + b_x)); 
         w[j * 2 + 1] = 1.0f/b_x * ((a_x * b_x) / ( a_x + b_x));  
     }/*for(j = k / 2; j < k; j++)*/
 
-    for(i = 0; i < sizeY; i++)
+    for (int i = 0; i < sizeY; i++)
     {
-      for(j = 0; j < sizeX; j++)
+      for (int j = 0; j < sizeX; j++)
       {
-        for(ii = 0; ii < k; ii++)
+        for (int ii = 0; ii < k; ii++)
         {
-          for(jj = 0; jj < k; jj++)
+          for (int jj = 0; jj < k; jj++)
           {
             if ((i * k + ii > 0) && 
                 (i * k + ii < height - 1) && 
                 (j * k + jj > 0) && 
                 (j * k + jj < width  - 1))
             {
-              d = (k * i + ii) * width + (j * k + jj);
+              int d = (k * i + ii) * width + (j * k + jj);
               (*map)->map[ i * stringSize + j * (*map)->numFeatures + alfa[d * 2    ]] += 
                   r[d] * w[ii * 2] * w[jj * 2];
               (*map)->map[ i * stringSize + j * (*map)->numFeatures + alfa[d * 2 + 1] + NUM_SECTOR] += 
@@ -260,10 +238,6 @@ int getFeatureMaps(const IplImage* image, const int k, CvLSVMFeatureMapCaskade *
         }/*for(ii = 0; ii < k; ii++)*/
       }/*for(j = 1; j < sizeX - 1; j++)*/
     }/*for(i = 1; i < sizeY - 1; i++)*/
-    
-    cvReleaseImage(&dx);
-    cvReleaseImage(&dy);
-
 
     free(w);
     free(nearest);
