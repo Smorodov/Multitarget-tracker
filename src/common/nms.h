5a448b5451bfca5e65122ddabf614d37c9c14db3
Nuzhny007 <nuzhny@mail.ru>
2020-08-31 07:46:40 +0300

    Small nms optimization

--- src/common/nms.h
+++ src/common/nms.h
@@ -10,26 +10,22 @@
  * @param thresh
  * @param neighbors
  */
-inline void nms(
-        const std::vector<cv::Rect>& srcRects,
-        std::vector<cv::Rect>& resRects,
-        float thresh,
-        int neighbors = 0
-        )
+inline void nms(const std::vector<cv::Rect>& srcRects,
+                std::vector<cv::Rect>& resRects,
+                float thresh,
+                int neighbors = 0)
 {
     resRects.clear();
 
     const size_t size = srcRects.size();
     if (!size)
-    {
         return;
-    }
 
     // Sort the bounding boxes by the bottom - right y - coordinate of the bounding box
     std::multimap<int, size_t> idxs;
     for (size_t i = 0; i < size; ++i)
     {
-        idxs.insert(std::pair<int, size_t>(srcRects[i].br().y, i));
+        idxs.emplace(srcRects[i].br().y, i);
     }
 
     // keep looping while some indexes still remain in the indexes list
@@ -64,9 +60,7 @@ inline void nms(
             }
         }
         if (neigborsCount >= neighbors)
-        {
             resRects.push_back(rect1);
-        }
     }
 }
 
@@ -79,22 +73,18 @@ inline void nms(
  * @param thresh
  * @param neighbors
  */
-inline void nms2(
-        const std::vector<cv::Rect>& srcRects,
-        const std::vector<float>& scores,
-        std::vector<cv::Rect>& resRects,
-        float thresh,
-        int neighbors = 0,
-        float minScoresSum = 0.f
-        )
+inline void nms2(const std::vector<cv::Rect>& srcRects,
+                 const std::vector<float>& scores,
+                 std::vector<cv::Rect>& resRects,
+                 float thresh,
+                 int neighbors = 0,
+                 float minScoresSum = 0.f)
 {
     resRects.clear();
 
     const size_t size = srcRects.size();
     if (!size)
-    {
         return;
-    }
 
     assert(srcRects.size() == scores.size());
 
@@ -102,7 +92,7 @@ inline void nms2(
     std::multimap<float, size_t> idxs;
     for (size_t i = 0; i < size; ++i)
     {
-        idxs.insert(std::pair<float, size_t>(scores[i], i));
+        idxs.emplace(scores[i], i);
     }
 
     // keep looping while some indexes still remain in the indexes list
@@ -122,8 +112,8 @@ inline void nms2(
             // grab the current rectangle
             const cv::Rect& rect2 = srcRects[pos->second];
 
-			float intArea = static_cast<float>((rect1 & rect2).area());
-			float unionArea = static_cast<float>(rect1.area() + rect2.area() - intArea);
+            float intArea = static_cast<float>((rect1 & rect2).area());
+            float unionArea = static_cast<float>(rect1.area() + rect2.area() - intArea);
             float overlap = intArea / unionArea;
 
             // if there is sufficient overlap, suppress the current bounding box
@@ -138,11 +128,8 @@ inline void nms2(
                 ++pos;
             }
         }
-        if (neigborsCount >= neighbors &&
-                scoresSum >= minScoresSum)
-        {
+        if (neigborsCount >= neighbors && scoresSum >= minScoresSum)
             resRects.push_back(rect1);
-        }
     }
 }
 
@@ -156,30 +143,26 @@ inline void nms2(
  * @param neighbors
  */
 template<typename OBJ, typename GET_RECT_FUNC, typename GET_SCORE_FUNC, typename GET_TYPE_FUNC>
-inline void nms3(
-        const std::vector<OBJ>& srcRects,
-        std::vector<OBJ>& resRects,
-        float thresh,
-        GET_RECT_FUNC GetRect,
-        GET_SCORE_FUNC GetScore,
-        GET_TYPE_FUNC GetType,
-        int neighbors = 0,
-        float minScoresSum = 0.f
-        )
+inline void nms3(const std::vector<OBJ>& srcRects,
+                 std::vector<OBJ>& resRects,
+                 float thresh,
+                 GET_RECT_FUNC GetRect,
+                 GET_SCORE_FUNC GetScore,
+                 GET_TYPE_FUNC GetType,
+                 int neighbors = 0,
+                 float minScoresSum = 0.f)
 {
     resRects.clear();
 
     const size_t size = srcRects.size();
     if (!size)
-    {
         return;
-    }
 
     // Sort the bounding boxes by the detection score
     std::multimap<float, size_t> idxs;
     for (size_t i = 0; i < size; ++i)
     {
-        idxs.insert(std::pair<float, size_t>(GetScore(srcRects[i]), i));
+        idxs.emplace(GetScore(srcRects[i]), i);
     }
 
     // keep looping while some indexes still remain in the indexes list
@@ -225,10 +208,7 @@ inline void nms3(
                 ++pos;
             }
         }
-        if (neigborsCount >= neighbors &&
-                scoresSum >= minScoresSum)
-        {
+        if (neigborsCount >= neighbors && scoresSum >= minScoresSum)
             resRects.push_back(srcRects[lastPos]);
-        }
     }
 }
