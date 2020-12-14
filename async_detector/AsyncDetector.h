377f3d8ad0673f4a0d9fb6c07c9d90c14462acc5
Nuzhny007 <nuzhny@mail.ru>
2020-12-14 14:54:49 +0300

    Refactoring work with frames queue

--- async_detector/AsyncDetector.h
+++ async_detector/AsyncDetector.h
@@ -27,11 +27,16 @@ struct FrameInfo
 	float m_fps = 0;
 	size_t m_frameInd = 0;
 
-    std::atomic<int> m_inDetector; // 0 - not in Detector, 1 - detector started processing, 2 - objects was detected
+	static constexpr int StateNotProcessed = 0;
+	static constexpr int StateInProcess = 1;
+	static constexpr int StateCompleted = 2;
+	static constexpr int StateSkipped = 3;
+
+    std::atomic<int> m_inDetector; // 0 - not in Detector, 1 - detector started processing, 2 - objects was detected, 3 - detector skip this frame
     std::atomic<int> m_inTracker;  // 0 - not in Tracker, 1 - tracker started processing, 2 - objects was tracked
 
     FrameInfo(size_t frameInd)
-        : m_frameInd(frameInd), m_inDetector(0), m_inTracker(0)
+        : m_frameInd(frameInd), m_inDetector(StateNotProcessed), m_inTracker(StateNotProcessed)
     {
     }
 };
