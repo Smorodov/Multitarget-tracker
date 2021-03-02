b6ba13e01216ab7221dc6f64097fc9f5d379732b
Nuzhny007 <nuzhny@mail.ru>
2021-03-02 06:31:16 +0300

    Flush results

--- example/VideoExample.cpp
+++ example/VideoExample.cpp
@@ -142,6 +142,8 @@ void VideoExample::SyncProcess()
 
 			WriteFrame(writer, frameInfo.m_frames[i].GetMatBGR());
 		}
+        if (framesCounter % 100 == 0)
+            m_resultsLog.Flush();
     }
 
     int64 stopLoopTime = cv::getTickCount();
@@ -242,6 +244,9 @@ void VideoExample::AsyncProcess()
             break;
 
  		++processCounter;
+
+        if (processCounter % 100 == 0)
+            m_resultsLog.Flush();
     }
     stopCapture = true;
 
